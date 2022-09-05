cf41b8e285d5dad67a73daa220030a24e0b60db2
Titus Mumo <titusmumo@gmail.com>
2022-09-05 18:06:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+        this.speak = speak = () =>{
+            return `${name} says meow!`
+        }
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+        this.speak = speak = () =>{
+            return `${name} says woof!`
+        }
+    }
+}
+
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    } 
+    speak(){
+        return `It's me! ${this.name}, the parrot!`
+    }
+}
